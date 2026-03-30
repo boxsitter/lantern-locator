6aab5eb2dae4bba7b5760433f00618797a32bcc1
boxsitter <leyton.houck@gmail.com>
2026-03-29 22:46:03 -0700

    Cleanup

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import csv
 import sys
 
-# 1. Setup the input and output
 input_file = 'data.csv'
-output_dst = sys.stdout  # This sends everything to the terminal
+output = sys.stdout
 
 with open(input_file, mode='r', newline='') as f:
     reader = csv.DictReader(f)
 
-    # 2. Define the new structure
-    new_headers = reader.fieldnames + ['New_Column']
-    writer = csv.DictWriter(output_dst, fieldnames=new_headers)
+    new_headers = reader.fieldnames
+    writer = csv.DictWriter(output, fieldnames=new_headers)
 
-    # 3. Write the header and the data
     writer.writeheader()
     for row in reader:
-        # This is where you add your logic
-        row['New_Column'] = 'Some Value'
         writer.writerow(row)
